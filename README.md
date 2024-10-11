c008cc0dd82f4837b9d1b4b4e9bac3bc38d0b0d1
vToshio <vtoshio2004@gmail.com>
2024-10-11 13:15:47 -0300

    docs: adição do README.md, .gitignore e index.js com resolução do desafio.

---
+++ README.md
@@ -0,0 +1,31 @@
+# Desafio de Lógica
+
+Desafio de programação proposto durante o curso de Lógica de Programação da [Digital Innovation Technology](https://www.dio.me/), pelo instrutor [Felipe Aguiar](https://www.linkedin.com/in/felipeaguiar-exe/).
+
+Curso iniciado durante a minha participação no [Bootcamp GFT Start #6 - Lógica de Programação](https://web.dio.me/track/gft-start-logica-de-programacao).
+
+
+### O que deve Ser Utilizado?
+- Variáveis
+- Operadores
+- Estruturas de Decisão e Repetição
+
+### Objetivo
+Crie uma varíavel para armazenar o nome e a quantidade de experiência(XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo: 
+
+```
+Se XP for menor do que 1.000 = Ferro
+Se XP for entre 1.001 e 2.000 = Bronze
+Se XP for entre 2.001 e 5.000 = Prata
+Se XP for entre 5.001 e 7.000 = Ouro
+Se XP for entre 7.001 e 8.000 = Platina
+Se XP for entre 8.001 e 9.000 = Ascendente
+Se XP for entre 9.001 e 10.000 = Imortal
+Se XP for maior ou igual a 10.001 = Radiante
+
+```
+### Saída
+Ao final, deve-se exibir a mensagem:
+```
+O Herói de nome {nome} está no nível de {nível}
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+//  Define a importação da biblioteca prompt-sync
+//e função de prompt para obter o input do usuário.
+'use strict';
+const prompt = require('prompt-sync')({sigint: true});
+
+// Função que define o nível do Herói
+function defineLevel(xp) {
+    let level;
+
+    if (xp<=1000) {
+        level = 'Ferro'
+    } else if (xp>1000 && xp<=2000) {
+        level = 'Bronze';
+    } else if (xp>2000 && xp<=5000) {
+        level = 'Prata';
+    } else if (xp>5000 && xp<=7000) {
+        level = 'Ouro';
+    } else if (xp>7000 && xp<=8000) {
+        level = 'Platina';
+    } else if (xp>8000 && xp<=9000) {
+        level = 'Ascendente';
+    } else if (xp>9000 && xp<=10000) {
+        level = 'Imortal';
+    } else {
+        level = 'Radiante';
+    }
+
+    return level;
+}
+
+// Execução da parte principal do programa
+while (true) {
+    let nome = prompt('Insira o nome do herói: ');
+    let xp = parseInt(prompt(`Insira a quatidade de XP do ${nome}: `));
+    
+    console.log(`O Herói de nome ${nome} está no nível de ${defineLevel(xp)}\n`);
+
+    if (parseInt(prompt('Deseja continuar as operações? (1=Sim, 2=Não): ')) == 2)
+        break;
+}
